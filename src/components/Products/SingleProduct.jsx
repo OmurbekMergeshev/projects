5c5f6cca367fd535c1fddeb5f49b1f5089efbdf3
Omar <vip.mergeshev@gmail.com>
2023-04-29 20:44:24 +0600

    add projects

---
+++ src/components/Products/SingleProduct.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useGetProductQuery } from '../../features/app/apiSlice'
+
+import { ROUTES } from '../../utils/Routes'
+import Product from './Product'
+import Products from './Products'
+import { useDispatch, useSelector } from 'react-redux'
+import { getRelatedProducts } from '../../features/products/productsSlice'
+
+function SingleProduct() {
+  const dispatch = useDispatch()
+  const {list, related} = useSelector(({ products }) => products)
+  const { id } = useParams()
+  const navigate = useNavigate()
+
+  const { data, isLoading, isFetching, isSuccess } = useGetProductQuery({ id })
+  useEffect(() => {
+    if (!isFetching && !isLoading && !isSuccess) {
+      navigate(ROUTES.HOME);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoading, isFetching, isSuccess]);
+
+  useEffect(() => {
+   if (!data || !list.length) return 
+    dispatch(getRelatedProducts(data.category.id))
+  }, [data, dispatch, list])
+
+  return !data ? (
+    <section className="preloader">Loading...</section>
+  ) : (
+    <>
+      <Product {...data} />
+      <Products products={related} amount={10} title="Related products" />
+    </>
+  )
+}
+
+export default SingleProduct
